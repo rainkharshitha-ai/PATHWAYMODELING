284ef54872aabc7dfc5f02820922cb5f8e66eed2
Harshitha Rai <rainkharshitha.ai@gmail.com>
2026-02-02 03:19:42 -0500

    Update image path to Image_temp

--- src/components/BecomeModel.jsx
+++ src/components/BecomeModel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";   // ✅ ADD THIS
-import ModelImg from "../Images/modeling-banner.jpg";
+import ModelImg from "../Image_temp/modeling-banner.jpg";
 
 
 const BecomeModel = () => {
